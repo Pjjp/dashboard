0d5cd3a29dc22a4bbf87d646515a5e8e391129c0
pjjp <p.j.paszko@gmail.com>
2025-03-23 13:51:17 +0100

    Prepare app for deployment

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
     return (
         <div>
-            <h1>Hello, React is working!</h1>
+            <h1>Hello, React is working! - say yes</h1>
         </div>
     );
 }
